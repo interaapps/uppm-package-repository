e5e5598c0b32d53dbcd0471e62e91080aa848bcf
Julian Gojani <julian@gojani.xyz>
2021-10-05 22:30:33 +0200

    Initial

--- resources/migrations/migration_211005_201040_create_organisation_users.php
+++ resources/migrations/migration_211005_201040_create_organisation_users.php
@@ -10,16 +10,17 @@ use de\interaapps\ulole\orm\migration\Migration;
  */
 class migration_211005_201040_create_organisation_users implements Migration {
     public function up(Database $database) {
-        return $database->create("users", function (Blueprint $blueprint) {
+        return $database->create("uppm_packages_organisation_users", function (Blueprint $blueprint) {
             $blueprint->id();
-            $blueprint->string("name")->default('John');
-            $blueprint->enum("gender", ["FEMALE", "MALE", "OTHER", "NO_ANSWER"])->default('NO_ANSWER');
+            $blueprint->int("organisation_id");
+            $blueprint->int("user_id");
+            $blueprint->enum("type", ["MEMBER", "ADMIN", "OWNER"]);
             $blueprint->timestamp("created_at")->currentTimestamp();
         });
     }
 
     public function down(Database $database) {
-        return $database->drop("users");
+        return $database->drop("uppm_packages_organisation_users");
         
     }
 }
--- resources/migrations/migration_211005_201053_create_package.php
+++ resources/migrations/migration_211005_201053_create_package.php
@@ -12,10 +12,9 @@ class migration_211005_201053_create_package implements Migration {
     public function up(Database $database) {
         return $database->create("uppm_packages_packages", function (Blueprint $blueprint) {
             $blueprint->id();
+            $blueprint->string("name");
             $blueprint->int("organisation_id");
-            $blueprint->int("user_id");
-            $blueprint->string("download_url");
-            $blueprint->enum("type", ["MEMBER", "ADMIN", "OWNER"]);
+            $blueprint->string("github");
             $blueprint->timestamp("created_at")->currentTimestamp();
         });
     }
