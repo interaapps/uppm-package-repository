269571a94058e62ed9b4b84e26e7543668444cc1
Julian Gojani <julian@gojani.xyz>
2021-10-05 22:34:05 +0200

    Initial

--- src/main/com/example/myproject/controller/OrganisationController.php
+++ src/main/com/example/myproject/controller/OrganisationController.php
@@ -2,20 +2,24 @@
 namespace com\example\myproject\controller;
 
 use com\example\myproject\model\Organisation;
+use com\example\myproject\responses\ErrorResponse;
 use de\interaapps\ulole\router\attributes\Controller;
 use de\interaapps\ulole\router\attributes\Route;
 use de\interaapps\ulole\router\Request;
 use de\interaapps\ulole\router\Response;
 
-#[Controller()]
+#[Controller]
 class OrganisationController {
     #[Route("/", method: "GET")]
     public static function getOrgas(Request $req, Response $resp){
-        return array_map(fn($o)=>$o->createResponse(), Organisation::table()->all());
+        $resp->json(array_map(fn($o)=>$o->createResponse(), Organisation::table()->all()));
     }
 
     #[Route("/([a-zA-Z0-9_-]+)", method: "GET")]
     public static function getOrga(Request $req, Response $resp, $name){
-        return Organisation::table()->where("name", $name)->get()->createResponse();
+        $orga = Organisation::table()->where("name", $name)->get();
+        if ($orga == null)
+            return ErrorResponse::notFound();
+        return $orga->createResponse();
     }
 }
